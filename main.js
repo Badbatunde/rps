2f200d0056327cdd06d24f1b34abb64af42f7a2a
Badbatunde <hb.oyesile@gmail.com>
2022-06-13 08:51:50 +0100

    Added playRound function to determine result of different input from the user and computerPlay

--- main.js
+++ main.js
@@ -5,5 +5,34 @@ function computerPlay () {
     return computerRandom   
 }
 
-let computerList = ['Rock', 'Paper', 'Scissors']
+let computerList = ['rock', 'paper', 'scissors']
 console.log (computerPlay())
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection === 'paper') {
+    return 'You Lose! Paper beats Rock.';
+    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    return 'Oh! It is a tie.';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock trumps Scissors.';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'Oh! It is a tie.';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors cuts Paper.';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper covers Rock.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock trumps Scissors.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors cuts Paper.';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'Oh! It is a tie.';
+    } else {
+        return 'Wrong input! Input either rock, paper or scissors'
+    }
+}
+
+let playerSelection = 'rock';
+let computerSelection = computerPlay();
+let play = playRound (playerSelection, computerSelection);
+ console.log (play)
